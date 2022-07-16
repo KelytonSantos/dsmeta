f0e03bcfb514072d464e83726b94d5da3040de10
lucas santos <kelytonlucas@gmail.com>
2022-07-16 12:10:37 -0300

    Sale Listing

---
+++ frontend/src/utils/request.ts
@@ -0,0 +1,2 @@
+export const BASE_URL = import.meta.env.VITE_BACKEND_URL ?? "http://localhost:8080";/*estou exportando uma variavel
+BASE_URL caso não tenha a url por padrão ele vai mandar a http://locahost:8080*/
